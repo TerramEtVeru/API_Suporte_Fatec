c1068575f6ac960715964a68ba7ba80b6e395f49
LuisSCardoso <luis200_cpv@hotmail.com>
2022-10-10 00:20:43 -0300

    Entrega 2

--- app.py
+++ app.py
@@ -6,43 +6,108 @@ import sqlite3 as sql
 app = Flask(__name__)
 
 #conn = sql.connect('database.db')
-#conn.execute('DROP TABLE reports')
-#conn.execute('CREATE TABLE reports (id INTEGER PRIMARY KEY AUTOINCREMENT, data TEXT, pc TEXT, sala TEXT, desc TEXT)')
+#conn.execute('DROP TABLE salas')
+#conn.execute('CREATE TABLE salas (nome TEXT, elements TEXT, total INTEGER)')
 #conn.close()
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', msg = "")
+@app.route('/home')
+def home():
+   return render_template('/home.html')
 
-@app.route('/admin')
-def admin():
-    con = sql.connect("database.db")
-    con.row_factory = sql.Row
-    cur = con.cursor()
-    cur.execute("SELECT * FROM reports")
-    rows = cur.fetchall()
-    return render_template('admin.html', username='', rows = rows)
+@app.route('/report')
+def report():
+   return render_template('/report.html')
+
+@app.route('/index', methods = ['POST', 'GET'])
+def index():
+   con = sql.connect("database.db")
+   con.row_factory = sql.Row
+   cur = con.cursor()
+   cur.execute("SELECT nome FROM salas")
+   rows = cur.fetchall()
+   return render_template('index.html', rows = rows)
 
 @app.route('/address', methods = ['POST', 'GET'])
 def address():
+   elmnts = ""
    if request.method == 'POST':
       try:
-         pc = request.form['pc']
-         sala = request.form['sala']
-         desc = request.form['desc']
-         with sql.connect("database.db") as con:
-            cur = con.cursor()
-            cur.execute("INSERT INTO reports (data,pc,sala,desc) VALUES (datetime('now'),?,?,?)",(pc,sala,desc))
-            con.commit()
-            msg = "Enviado com sucesso"
+         selected = request.form["selectedvalue"]
+
+         if (request.form['actiontype'] == "add"):
+            nome = request.form['txtnew']
+            elements = request.form['elementcontent']
+            total = request.form['totalcontent']
+            with sql.connect("database.db") as con:
+               cur = con.cursor()
+               cur.execute('INSERT INTO salas (nome, elements, total) VALUES (?, ?, ?)',(nome, elements, total))
+               con.commit()
+               msg = "Sala criada com sucesso"
+
+         elif (request.form['actiontype'] == "del"):
+            nome = request.form['salalist']
+            with sql.connect("database.db") as con:
+               cur = con.cursor()
+               cur.execute("DELETE FROM salas WHERE nome="+nome)
+               con.commit()
+               msg = "Sala delatada com sucesso"
+
+         elif (request.form['actiontype'] == "save"):
+            nome = request.form['salalist']
+            elements = request.form['elementcontent']
+            total = request.form['totalcontent']
+            with sql.connect("database.db") as con:
+               cur = con.cursor()
+               cur.execute("UPDATE salas SET elements = ?, total = ? WHERE nome="+nome,(elements, total))
+               con.commit()
+               msg = "Sala modificada com sucesso"
+
+               nome = request.form['salalist']
+               with sql.connect("database.db") as con:
+                  con.row_factory = sql.Row
+                  cur = con.cursor()
+                  cur.execute("SELECT * FROM salas WHERE nome="+nome)
+                  elmnts = cur.fetchall()
+
+         elif (request.form['actiontype'] == "load"):
+            nome = request.form['salalist']
+            with sql.connect("database.db") as con:
+               con.row_factory = sql.Row
+               cur = con.cursor()
+               cur.execute("SELECT * FROM salas WHERE nome="+nome)
+               elmnts = cur.fetchall()
+               msg = "Sala carregada com sucesso"
+
       except:
          con.rollback()
-         msg = "ERRO: ENTRAR EM CONTATO COM OS DESENVOLVEDORES"
-      
+         msg = ("ERRO")
+
       finally:
-         return render_template("index.html", msg = msg)
+         con = sql.connect("database.db")
+         con.row_factory = sql.Row
+         cur = con.cursor()
+         cur.execute("SELECT nome FROM salas")
+         rows = cur.fetchall()
+         return render_template("index.html", rows = rows, selected = selected, msg = msg, elmnts = elmnts)
          con.close()
+
+@app.route('/sala')
+def sala():
+      nome = "302"
+      #carregar todas as salas, e depois de carregar elas
+      with sql.connect("database.db") as con:
+         con.row_factory = sql.Row
+         cur = con.cursor()
+         cur.execute("SELECT * FROM salas")
+         rows = cur.fetchall()
+         msg = "Sala carregada com sucesso"
+         return render_template("sala.html", rows = rows)
+         con.close()
+         
+@app.route('/admin')
+def admin():
+   return render_template('admin.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
